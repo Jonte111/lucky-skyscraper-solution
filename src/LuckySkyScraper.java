bd0e8ee80b6c21c1a3726e02c0f6c8848d75ba26
Jonte111 <jonathan.pe@telia.com>
2020-09-29 10:58:20 +0200

    Looping through real floor numbers

--- src/LuckySkyScraper.java
+++ src/LuckySkyScraper.java
@@ -1,3 +1,8 @@
 public class LuckySkyScraper {
-
+    static public void floorList(int totalRealFloors){
+        //loop through real floor numbers
+        for(var realFloor=1; realFloor <=totalRealFloors; realFloor++){
+            System.out.println("realFloorNo:" + realFloor);
+        }
+    }
 }
