d857d75b326ca26ec9ea84d2440a865688cddeca
Jonte111 <jonathan.pe@telia.com>
2020-09-29 11:33:01 +0200

    Wrote the toRealFloorMethod

--- src/LuckySkyScraper.java
+++ src/LuckySkyScraper.java
@@ -1,5 +1,7 @@
 public class LuckySkyScraper {
-    static public void floorList(int totalRealFloors){
+
+    //basic logic
+   /* static public void floorList(int totalRealFloors){
         var fakeFloor = 1;
         //loop through real floor numbers
         for(var realFloor=1; realFloor <=totalRealFloors; realFloor++){
@@ -9,5 +11,21 @@ public class LuckySkyScraper {
             //fakeFloor ends with 4 or ends with 13
            }while(fakeFloor % 10 == 4 || fakeFloor % 100 == 13);
         }
+    }
+
+    */
+    static public int toRealFloor(int fakeFloorGoal){
+        var fakeFloor = 1;
+        //loop through real floor numbers
+        for(var realFloor=1; realFloor <= fakeFloorGoal; realFloor++){
+            if(fakeFloorGoal == fakeFloor){
+                return realFloor;
+            }
+            do{
+                fakeFloor++;
+                //fakeFloor ends with 4 or ends with 13
+            }while(fakeFloor % 10 == 4 || fakeFloor % 100 == 13);
+        }
+        return -1;//Could not find real floor number
     }
 }
