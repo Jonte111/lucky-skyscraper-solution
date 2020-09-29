fcff4e8b288d26a92694233958a10b5fe8aee147
Jonte111 <jonathan.pe@telia.com>
2020-09-29 11:45:17 +0200

    Finished it and tested

--- src/LuckySkyScraper.java
+++ src/LuckySkyScraper.java
@@ -28,4 +28,18 @@ public class LuckySkyScraper {
         }
         return -1;//Could not find real floor number
     }
+    static public int toFakeFloor(int realFloorGoal){
+        var fakeFloor = 1;
+        //loop through real floor numbers
+        for(var realFloor=1; realFloor < realFloorGoal; realFloor++){
+            if(realFloorGoal == fakeFloor){
+                return fakeFloor;
+            }
+            do{
+                fakeFloor++;
+                //fakeFloor ends with 4 or ends with 13
+            }while(fakeFloor % 10 == 4 || fakeFloor % 100 == 13);
+        }
+        return -1;//Could not calculate fake floor number
+    }
 }
